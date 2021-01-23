4c3cfaad441234b66ac679b75015650e584c8fac
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-01-23 12:27:21 +0100

    add vuetify

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,23 +1,17 @@
 <template >
   <div class="dashboard">
     <h1>Event Attendees</h1>
-    <pagination v-model="page" :records="100" :per-page="x25" />
-  </div>
+      </div>
 </template>
 <script>
 
-import MyPagination from '../components/pagination.vue'
 
 export default {
   data() {
     return {
-      page: 1,
-    };
+
+};
   },
-   options: {
-        template: MyPagination    
-    }   
-  
 };
 </script>
 <style>
