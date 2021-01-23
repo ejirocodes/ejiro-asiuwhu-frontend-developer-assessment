dd3abc05d29652c0db0e30b1fcafa50019de524b
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-01-23 12:18:09 +0100

    add fake json

---
+++ src/pages/dashboard.vue
@@ -0,0 +1,24 @@
+<template >
+  <div class="dashboard">
+    <h1>Event Attendees</h1>
+    <pagination v-model="page" :records="100" :per-page="x25" />
+  </div>
+</template>
+<script>
+
+import MyPagination from '../components/pagination.vue'
+
+export default {
+  data() {
+    return {
+      page: 1,
+    };
+  },
+   options: {
+        template: MyPagination    
+    }   
+  
+};
+</script>
+<style>
+</style>
